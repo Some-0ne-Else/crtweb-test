363914e21937403c5cd3760553f97f539e40da8d
Some-0ne-Else <cyberdef@gmail.com>
2021-04-29 00:31:10 +0300

    task 4 implementation

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const headerButton = document.querySelector('.header__button');
 const menu = document.querySelector('.menu');
 const menuLinkTextList = document.querySelectorAll(".menu__link-text");
-console.log(menuLinkTextList)
+const checkboxList = document.querySelectorAll(".task-four__checkbox");
+const textCheckbox = document.getElementById("text");
+const taskFourText = document.querySelector(".task-four__text");
+const buttonCheckbox = document.getElementById("button");
+const taskFourButton = document.querySelector(".task-four__button");
+const imageCheckbox = document.getElementById("image");
+const taskFourImage = document.querySelector(".task-four__image");
+const checkallCheckbox = document.getElementById("checkall");
+
+const checkState = (checkboxList) => {
+    const checkedNow = document.querySelectorAll(".task-four__checkbox:checked");
+    if (checkedNow.length > 0 && checkedNow.length <= 2) { checkallCheckbox.indeterminate = true }
+    if (checkedNow.length === 0) { checkallCheckbox.indeterminate = false; checkallCheckbox.checked = false; }
+    if (checkedNow.length === 3) { checkallCheckbox.indeterminate = false; checkallCheckbox.checked = true; }
+}
+
 
 headerButton.addEventListener('click', () => {
     menu.classList.toggle("menu_hidden");
-    menuLinkTextList.forEach((el) => { el.classList.toggle("menu__link-text_hidden") })
+    menuLinkTextList.forEach((el) => { el.classList.toggle("menu__link-text_hidden"); })
 })
 window.addEventListener('resize', () => {
     if (window.innerWidth >= 1200) {
@@ -15,5 +30,25 @@ window.addEventListener('resize', () => {
     else {
         menu.classList.add("menu_hidden")
         menuLinkTextList.forEach((el) => { el.classList.add("menu__link-text_hidden") })
+    }
+})
+
+textCheckbox.addEventListener('click', () => { taskFourText.classList.toggle("task-four__text_visible"); checkState(checkboxList); });
+buttonCheckbox.addEventListener('click', () => { taskFourButton.classList.toggle("task-four__button_visible"); checkState(checkboxList); });
+imageCheckbox.addEventListener('click', () => { taskFourImage.classList.toggle("task-four__image_visible"); checkState(checkboxList); });
+checkallCheckbox.addEventListener('click', () => {
+
+    if (checkallCheckbox.checked === true) {
+        checkboxList.forEach((el) => el.checked = true);
+        taskFourText.classList.add("task-four__text_visible");
+        taskFourButton.classList.add("task-four__button_visible");
+        taskFourImage.classList.add("task-four__image_visible");
+
+    }
+    else {
+        checkboxList.forEach((el) => el.checked = false);
+        taskFourText.classList.remove("task-four__text_visible");
+        taskFourButton.classList.remove("task-four__button_visible");
+        taskFourImage.classList.remove("task-four__image_visible");
     }
 })
